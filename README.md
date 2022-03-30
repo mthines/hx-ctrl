bacf3494125f489bbce42bb04c543b282242fe99
mthines <mads.thines@noaignite.com>
2022-03-30 21:54:17 +0200

    feat: hx_ctrl default generator + loader working

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hx-ctrl
-DAW Midi Controller for the Line 6 Helix (jsfx)
+# HX Ctrl
+
+A DAW Midi Controller for the Line 6 Helix (jsfx)
+
+@author Mads Thines Coello @mthines
+
+A MIDI Controller, for sending specific CC signals to the Line 6 Helix
+
+Inspired by https://stash.reaper.fm/v/3763/Automation_to_MIDI.zip
---
+++ src/scripts/loader.ts
@@ -0,0 +1,110 @@
+import { Preset, ToneBlockTypes } from '@src/types/helix';
+import { getArg } from '@src/utils/process';
+import { Config, generatePlugin, Slider, SLIDER_DEFAULTS } from '@src/utils/slider';
+import * as fs from 'fs';
+
+const filePath = getArg('path', {
+  required: true,
+  errorMessage: [
+    'The path to the *.hlx file is required',
+    'Please add the path to your file (e.g. --path=./path/to/your/preset.hlx)',
+  ],
+});
+
+const outputDir = getArg('output');
+
+const getSliderFromPreset = async (): Promise<{
+  config: Config;
+  sliders: Slider[];
+} | void> => {
+  try {
+    const fileName = '02_03_22-CC.hlx';
+    const preset = fs.readFileSync(filePath, { encoding: 'utf-8' });
+    const parsed = JSON.parse(preset) as Preset;
+    const {
+      data: { meta, tone },
+    } = parsed;
+    const { controller } = tone;
+    const dsps = Object.entries(controller);
+
+    const config: Config = {
+      // desc: `HX Ctrl - ${meta.name} (${dayjs().format('HH:mm:ss DD/MM/YYYY')})`,
+      desc: `HX Ctrl - ${meta.name}`,
+      outputDir,
+      fileName,
+    };
+
+    const slidersRecord: Record<string, Slider> = {};
+
+    dsps.forEach(([dspName, dsp]) => {
+      const toneBlocks = tone?.[dspName as keyof typeof tone] as ToneBlockTypes;
+      const blocks = Object.entries(dsp);
+
+      blocks.forEach(([blockName, block]) => {
+        const properties = Object.entries(block);
+
+        const toneBlock = toneBlocks?.[blockName];
+        properties.forEach(([propertyName, property]) => {
+          if (!property['@cc']) return;
+
+          const isSwitch = property['@max'] === true || property['@max'] === false;
+
+          if (isSwitch) {
+            slidersRecord[property['@cc']] = {
+              cc: property['@cc'],
+              title: `${propertyName} (${toneBlock['@model']})`,
+              max: property['@max'] === true ? 1 : 0,
+              min: property['@max'] === true ? 0 : 1,
+              default: 0,
+            };
+            return;
+          }
+
+          slidersRecord[property['@cc']] = {
+            cc: property['@cc'],
+            title: `${propertyName} (${toneBlock['@model']})`,
+            max: 127,
+            min: 0,
+            default: 0,
+          };
+        });
+      });
+    });
+
+    const collectedSlides: typeof slidersRecord = {
+      ...slidersRecord,
+      10: {
+        ...slidersRecord[10],
+        default: 102,
+      },
+      11: {
+        ...slidersRecord[11],
+        default: 63,
+      },
+    };
+
+    const slidersEntries = Object.values(collectedSlides);
+
+    slidersEntries.sort((a, b) => {
+      if (!a.cc || !b.cc) return 0;
+      return a.cc - b.cc;
+    });
+
+    return { config, sliders: [SLIDER_DEFAULTS.SNAPSHOT, ...slidersEntries] };
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const load = async (): Promise<void> => {
+  const { config, sliders } = (await getSliderFromPreset()) || {};
+
+  if (!sliders || !config) {
+    console.warn('Not able to generate sliders from the Preset');
+    return;
+  }
+
+  generatePlugin(sliders, config);
+};
+
+load();

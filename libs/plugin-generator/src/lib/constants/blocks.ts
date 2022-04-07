69f54f7226a00d096e4ca41b0091152fb28cb44b
mthines <mads.thines@noaignite.com>
2022-04-07 09:18:39 +0200

    fix(constants/blocks): typo

--- libs/plugin-generator/src/lib/constants/blocks.ts
+++ libs/plugin-generator/src/lib/constants/blocks.ts
@@ -4,7 +4,7 @@ export const blocknames: Record<Block['@model'], string> = {
   HD2_AppDSPFlowOutput: 'Output',
   VIC_DynPlate: 'Dynamic Hall',
   HD2_VolPanStereoWidth: 'Stereo Width',
-  HD2_DL4DynamicDelayStereo: 'Dymamic Delay (Legacy)',
+  HD2_DL4DynamicDelayStereo: 'Dynamic Delay (Legacy)',
   HD2_RetroReel: 'Retro Reel',
   L6SPB_PolyWham: 'Poly Wham',
   HD2_VolPanGain: 'Gain',
